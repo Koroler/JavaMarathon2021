d161ebe833ee8aa0a78a2755d89b2406ead157f6
Erik Korol <koroler@ro.ru>
2021-01-22 15:39:55 +0300

    Add Day3 Solution

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,41 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean cond = true;
+        while (cond){
+            String city = scanner.nextLine();
+        switch (city) {
+            case ("Москва"):
+            case ("Владивосток"):
+            case ("Ростов"):
+                System.out.println("Россия");
+                break;
+            case ("Рим"):
+            case ("Милан"):
+            case ("Турин"):
+                System.out.println("Италия");
+                break;
+            case ("Ливерпуль"):
+            case ("Манчестер"):
+            case ("Лондон"):
+                System.out.println("Англия");
+                break;
+            case ("Берлин"):
+            case ("Мюнхен"):
+            case ("Кёльн"):
+                System.out.println("Германия");
+                break;
+            case ("Stop"):
+                cond = false; // Вероятно решение неканоничное... но работает, тем не менее.
+                break;
+            default:
+                System.out.println("Неизвестная страна");
+        }
 
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i < 5){
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if(b == 0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+            i++;
+        }
     }
 }
