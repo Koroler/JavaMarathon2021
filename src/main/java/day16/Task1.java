d53fb1eed6d5e5ba58f7c160375cfddd11e887e1
Erik Korol <koroler@ro.ru>
2021-02-07 21:42:47 +0300

    Add Day16 Solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,37 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("testFile2.txt"); //сделал файл отличный от файла задания 14.1, чтобы могла получиться бесконечная дробь
+        printResult(file);
 
     }
+
+    public static void printResult(File file){
+        try {
+            Scanner scanner = new Scanner(file);
+
+            int sum = 0; int i = 0;
+            while (scanner.hasNextInt()) {
+                sum += scanner.nextInt();
+                i++;
+            }
+
+            double average = (double) sum / i;
+            System.out.printf("%f --> %.3f", average, average);
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+    }
+
 }
+
+
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,49 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Random random = new Random();
+
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+
+        PrintWriter pw1 = new PrintWriter(file1);
+        for (int i = 0; i < 1000; i++) {
+            pw1.println(random.nextInt(101));
+        }
+        pw1.close();
+
+        PrintWriter pw2 = new PrintWriter(file2);
+
+        Scanner scanner = new Scanner(file1);
+        int sum = 0;
+        int i = 0;
+        while (scanner.hasNextInt()){
+            sum += scanner.nextInt();
+            i++;
+            if(i == 20){
+                pw2.println((double) sum / i);
+                sum = 0;
+                i = 0;
+            }
+        }
+        scanner.close();
+        pw2.close();
+        printResult(file2);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        double sum = 0;
+        while (scanner.hasNextDouble()){
+            sum += scanner.nextDouble();
+        }
+        System.out.println((int)sum);
     }
 }
