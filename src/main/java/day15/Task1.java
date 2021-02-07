ebd26a54d037637a3a813c3581582387af9b7321
Erik Korol <koroler@ro.ru>
2021-02-07 17:59:14 +0300

    Add Day15 Solution

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,44 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> missingShoes = new ArrayList<>();
+        File importFile = new File("E:\\Eldritch\\JavaMarathon2021\\src\\main\\resources\\shoes.csv");
+        try {
+            Scanner scanner = new Scanner(importFile);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] data = line.split(";");
+                if(Integer.parseInt(data[2]) == 0){
+                    missingShoes.add(line);
+                }
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Импортируемый файл не найден");
+        }
+
+
+        File exportFile = new File("E:\\Eldritch\\JavaMarathon2021\\src\\main\\resources\\missing_shoes.txt");
+        try {
+            PrintWriter pw = new PrintWriter(exportFile);
+            for (String shoe : missingShoes){
+                pw.println(shoe);
+            }
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Экспортируемый файл не найден");
+        }
+
 
     }
 }
